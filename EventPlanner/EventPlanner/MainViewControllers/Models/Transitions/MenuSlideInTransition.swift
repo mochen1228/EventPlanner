4e9d5b6a2e56e94564e0ed7a71bd6855abf4e3c1
Chen Mo <mochen1228@gmail.com>
2020-01-29 19:39:31 -0500

    First commit

---
+++ EventPlanner/EventPlanner/MainViewControllers/Models/Transitions/MenuSlideInTransition.swift
@@ -0,0 +1,9 @@
+//
+//  MenuSlideInTransition.swift
+//  EventPlanner
+//
+//  Created by Hamster on 1/29/20.
+//  Copyright © 2020 Hamster. All rights reserved.
+//
+
+import Foundation
---
+++ EventPlanner/EventPlanner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EventPlanner
+//
+//  Created by Hamster on 1/29/20.
+//  Copyright © 2020 Hamster. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
